15ba77d6fbc4e04b0885d10351c2413b7f4535f8
ballareka <ballareka17@gmail.com>
2018-04-22 20:59:18 +0200

    folytatás

--- 7.feladat.py
+++ 7.feladat.py
@@ -1,13 +1,32 @@
-def leghosszabb(string):
+def lps(string):
     maxLength=1
 
     start=0
-    length=len(string)
+    hossz=len(string)
 
     low=0
     high=0
 
-    for i in range(1,length):
+    for i in range(1,hossz):
         low=i-1
         high=i
-        
+
+        while low>=0 and high<hossz and string[low]==string[high]:
+            if high-low+1 > maxLength:
+                start=low
+                maxLength=high-low+1
+            low-=1
+            high+=1
+
+        low=i-1
+        high=i+1
+        while low>=0 and high<hossz and string[low]==string[high]:
+            if high-low+1 >maxLength:
+                start=low
+                maxLength=high-low+1
+            low-=1
+            high3=1
+    print("leghosszabb palindrome: ")
+    print(string[start:start+maxLength])
+    return maxLength
+print(lps("okgdadok"))
