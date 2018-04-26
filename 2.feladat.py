edbe18723fc0c84d59cf207451dce077db1049a9
Ballareka <36822862+Ballareka@users.noreply.github.com>
2018-04-26 21:43:42 +0200

    Update 2.feladat.py

--- 2.feladat.py
+++ 2.feladat.py
@@ -1,4 +1,16 @@
-b1="kettes számrendszer"
-b2="tizes számrendszer"
-A=[1100,10101,11001]
+b1=2
+b2=10
+A=[100,101,1001]
 B=[]
+
+def binaryToDecimal(binary):
+    for binary in binary[:]:
+        decimal, i = 0, 0
+        while (binary != 0):
+            dec = binary % 10
+            decimal = decimal + dec * pow(b1, i)
+            binary = binary // 10
+            i += 1
+        B.append(decimal)
+    print(B)
+binaryToDecimal(A)
