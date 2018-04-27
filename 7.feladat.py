7e3feb6116761d24d6aada3b1acaee16a0bb5e32
Ballareka <36822862+Ballareka@users.noreply.github.com>
2018-04-27 20:32:35 +0200

    Update 7.feladat.py

--- 7.feladat.py
+++ 7.feladat.py
@@ -8,15 +8,6 @@ def lps(string):
     high=0
 
     for i in range(1,hossz):
-        low=i-1
-        high=i
-
-        while low>=0 and high<hossz and string[low]==string[high]:
-            if high-low+1 > maxLength:
-                start=low
-                maxLength=high-low+1
-            low-=1
-            high+=1
 
         low=i-1
         high=i+1
