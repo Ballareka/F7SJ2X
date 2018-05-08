85c50b9f0f58a9490688fd95cfcf3e4aea726d1d
Ballareka <36822862+Ballareka@users.noreply.github.com>
2018-05-08 18:04:58 +0200

    Update 5.feladat.py

--- 5.feladat.py
+++ 5.feladat.py
@@ -3,3 +3,13 @@ combinations=pow[3,len(digits)-1]
 
 solutions=[]
 ops=["", "-","+"]
+
+def convertToBase3(x):                           # a számjegyekhez az összes lehetőségbeli operátort hozzá kell rendelni,
+    number,i=0,0                                 # ezért a ciklusváltozót átváltjuk 3-as számrendszerbe
+    while(x !=0):
+        modulo = x % 3
+        number=number + modulo * pow(10,i)
+        x=x //3
+        i+=1
+    return number
+
