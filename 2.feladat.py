0b5dbdc75a62c8d406f1d4a6a1273dc1a9eed092
Ballareka <36822862+Ballareka@users.noreply.github.com>
2018-05-09 15:38:52 +0200

    Update 2.feladat.py

--- 2.feladat.py
+++ 2.feladat.py
@@ -1,14 +1,14 @@
-A=[45]
+A=[20]
 B=[]
 
-def convert(numbers, b1, b2):
-    for numbers in numbers[:]:
+def convert(numbers, b1, b2):                              # először a megadott számot átváltom 10-es
+    for numbers in numbers[:]:                             # számrendszerbe, majd utána a megadott b2-be
         decimal, i = 0, 0
         while (numbers != 0):
-            dec = numbers % 10
-            decimal = decimal + dec * pow(b1, i)
-            numbers = numbers // 10
-            i += 1
+            dec = numbers % 10                             # maradékosan osztjuk a számot tízzel, mert mindig az
+            decimal = decimal + dec * pow(b1, i)           # utolsó számjegyet emeljük hatványra
+            numbers = numbers // 10                        # ezután újra elosztjuk tízzel, hogy a következő számjegyre
+            i += 1                                         # tudjon lépni 
 
         number, i =0,0
         while(decimal !=0):
@@ -18,4 +18,7 @@ def convert(numbers, b1, b2):
             i+=1
         B.append(number)
     print(B)
-convert(A,6,10)
+convert(A,3,2)
+
+
+
