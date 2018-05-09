5c2291a587dac2c613785ba2b72dd2fa9d2e2562
Ballareka <36822862+Ballareka@users.noreply.github.com>
2018-05-09 17:36:43 +0200

    Update 7.feladat.py

--- 7.feladat.py
+++ 7.feladat.py
@@ -1,23 +1,52 @@
 def lps(string):
     maxLength=1
 
-    start=0
-    hossz=len(string)
+    start=0                                                          # a startba elmentjük a leghosszabb megtalált
+    hossz=len(string)                                                # palindrom kezdeti pozícióját
 
-    low=0
-    high=0
-
+    low=0                                                             # a for ciklus éppan ahanyadik karakternél jár
+    high=0                                                            # a low az azelőtti, a high pedig az azutáni
+                                                                      # karaktert fogja jelölni
     for i in range(1,hossz):
-
-        low=i-1
+        low=i                                                         # az első rész a páros részekre van
         high=i+1
-        while low>=0 and high<hossz and string[low]==string[high]:
-            if high-low+1 >maxLength:
-                start=low
-                maxLength=high-low+1
-            low-=1
-            high3=1
+        if low > 0 and string[low].isspace():                         # ha az éppen vizsgált karakter alsó vagy felső
+            low -= 1                                                  # részén szóköz van, akkor az eggyel mellette
+        if high < hossz - 1 and string[high].isspace():               # lévőt kell vizsgálni
+            high += 1                                                 # itt csak a kezdőpozíción nézi meg egyszer
+        while low >= 0 and high < hossz and string[low].lower() == string[high].lower():
+            if high - low + 1 > maxLength:                            # megvizsgáljuk, hogy aktuális karekter előtt meg
+                start = low                                           # után lévő karakterek meddig egyeznek
+                maxLength = high - low + 1
+            low -= 1
+            high += 1
+            if low > 0 and string[low].isspace():                     # újra meg kell vizsgálni, hogy van e szóköz
+                low -= 1
+            if high < hossz - 1 and string[high].isspace():
+                high += 1
+
+
+        low = i - 1                                                   # ez a második rész a páratlan részekre van
+        high = i + 1
+        if low > 0 and string[low].isspace():
+            low -= 1
+        if high < hossz - 1 and string[high].isspace():
+            high += 1
+        while low >= 0 and high < hossz and string[low].lower() == string[high].lower():
+            if high - low + 1 > maxLength:
+                start = low
+                maxLength = high - low + 1
+            low -= 1
+            high += 1
+            if low > 0 and string[low].isspace():
+                low -= 1
+            if high < hossz - 1 and string[high].isspace():
+                high += 1
+
+
+
     print("leghosszabb palindrome: ")
-    print(string[start:start+maxLength])
+    print(string[start:start + maxLength])
     return maxLength
-print(lps("okgdadok"))
+
+print(lps("A cápa ette apáca "))
